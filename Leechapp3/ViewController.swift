2141701516ae85e08bfa5bd16a28fb1e22694942
Joe Morris <joe.edward.morris@gmail.com>
2016-11-29 18:12:13 +0000

    Initial Commit

---
+++ Leechapp3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Leechapp3
+//
+//  Created by Joe Morris on 29/11/2016.
+//  Copyright © 2016 Joe Morris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
